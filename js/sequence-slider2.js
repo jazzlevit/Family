835a0dd80536e25758d1c3f7dc968e71aef6beda
oleg <oleg@MacBook-Pro-oleg.local>
2021-09-18 20:50:02 +0300

    Family init

---
+++ js/sequence-slider2.js
@@ -0,0 +1,25 @@
+(function($) {
+  "use strict";
+
+var $ = jQuery.noConflict();
+
+//SLIDER
+$(document).ready(function(){
+    var options = {
+        nextButton: true,
+        prevButton: true,
+        animateStartingFrameIn: true,
+		autoPlay: true,
+        autoPlayDelay: 4000,
+        preloader: true,
+        pauseOnHover: false,
+        preloadTheseFrames: [1],
+
+    };
+    
+    var sequence = $("#sequence").sequence(options).data("sequence");
+
+
+});
+
+})(jQuery);
